52f7624afcbb1673ddc7b53bf0b33d96ef8436cb
Imgone25 <Imgone25@gmail.com>
2022-08-06 22:40:22 -0500

    currently makes a list of all image URLs for a given board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from concurrent.futures import thread
+from concurrent.futures import ThreadPoolExecutor, thread
+from threading import Thread,local
+from requests.sessions import Session
 from abbreviations import BOARD_ABBREVIATIONS
 import requests, re, json
 
@@ -38,9 +40,6 @@ thread_json_url = []
 # for i in range(len(thread_num_list)):
 #     thread_json_url.append("https://a.4cdn.org/" + board_selection + "/thread/" + str(thread_num_list[i]) + ".json")
 
-# print(thread_json_url)
-
-# print(thread_num_list[0])
 
 for i in range(len(thread_num_list)):
     total_string = ''
@@ -48,4 +47,51 @@ for i in range(len(thread_num_list)):
     thread_json_url.append(total_string)
 
 # thread_json_url is now a list of every JSON api request for every thread on a board
-print(len(thread_json_url))
+
+thread_json_data = []
+#now we need to parse the new json_url with a loop
+
+thread_local = local()
+
+def get_session() -> Session:
+    if not hasattr(thread_local, 'session'):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def download_link(url:str):
+    session = get_session()
+
+    with session.get(url) as response:
+        thread_json_data.append(response.json())
+# print(thread_json_url)
+with ThreadPoolExecutor(max_workers=100) as executor:
+    executor.map(download_link, thread_json_url)
+
+image_link_list = []
+# print(thread_json_data[0]["posts"][0]["tim"])
+def check_for_image(i, j):
+    try:
+        thread_json_data[i]["posts"][j]["tim"]
+        return True
+    except:
+        return False
+
+for i in range(len(thread_json_data)):
+    for j in range(len(thread_json_data[i]["posts"])):
+        # print(j)
+        if(check_for_image(i,j)):
+            img_id = str(thread_json_data[i]["posts"][j]["tim"])
+            img_ext = thread_json_data[i]["posts"][j]["ext"]
+            # print("https://i.4cdn.org/"+board_selection+"/"+img_id+img_ext)
+            image_link_list.append("https://i.4cdn.org/"+board_selection+"/"+img_id+img_ext)
+        else:
+            continue
+        
+
+# download all images
+
+# for i in range(len(image_link_list)):
+
+#     img_data = requests.get(image_link_list[i]).content
+#     with open('')
+print(image_link_list)
